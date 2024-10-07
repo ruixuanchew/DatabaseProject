8242712b023efee9f81b96916d51bca57b1cd87d
Dogephin <64132341+Dogephin@users.noreply.github.com>
2024-10-07 12:11:55 +0800

    Updated details to display calculated nutritional value

--- public/js/details.js
+++ public/js/details.js
@@ -7,6 +7,10 @@ function getRecipeIdFromUrl() {
 // Function to dynamically display recipe details
 function displayRecipeDetails() {
     const recipeId = getRecipeIdFromUrl();
+    let TotalCal = 0;
+    let TotalProtein = 0;
+    let TotalFat = 0;
+    let TotalCarbs =0;
     
     if (!recipeId) {
         console.error('No recipe ID found in the URL');
@@ -22,6 +26,33 @@ function displayRecipeDetails() {
             return response.json();
         })
         .then(recipe => {
+             //Fetch nutrition value for each ingridient in recipe
+            fetch(`/nutritions/${recipeId}`)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error(`Nutritional data for ${recipe} not found`);
+                }
+                return response.json();
+            })
+            .then(nutrition => {
+                for(let i = 0; i<nutrition.length; i++){
+                    TotalCal += parseInt(nutrition[i].calories);
+                    TotalCarbs += parseInt(nutrition[i].carbs);
+                    TotalFat += parseInt(nutrition[i].fat);
+                    TotalProtein += parseInt(nutrition[i].protein);
+                }
+            // Populate the Nutritional Information section
+            const nutritionTable = document.getElementById('recipeNutrition');
+            nutritionTable.innerHTML = `
+                <tr><td>Calories</td><td>${TotalCal} kcal</td></tr>
+                <tr><td>Protein</td><td>${TotalProtein} g</td></tr>
+                <tr><td>Fat</td><td>${TotalFat} g</td></tr>
+                <tr><td>Carbs</td><td>${TotalCarbs} g</td></tr>
+            `;
+            })
+            .catch(error => {
+                console.error(`Error fetching nutrition data for ${recipe}:`, error);
+            });
             // Populate the Ingredients section
             const ingredientsList = document.getElementById('recipeIngredients');
             ingredientsList.innerHTML = '';
@@ -41,15 +72,6 @@ function displayRecipeDetails() {
                 li.textContent = step;
                 stepsList.appendChild(li);
             });
-
-            // Populate the Nutritional Information section
-            const nutritionTable = document.getElementById('recipeNutrition');
-            nutritionTable.innerHTML = `
-                <tr><td>Calories</td><td>${recipe.calories}</td></tr>
-                <tr><td>Protein</td><td>${recipe.protein} g</td></tr>
-                <tr><td>Fat</td><td>${recipe.fat} g</td></tr>
-                <tr><td>Carbs</td><td>${recipe.carbs} g</td></tr>
-            `;
         })
         .catch(error => console.error('Error fetching recipe details:', error));
 }
