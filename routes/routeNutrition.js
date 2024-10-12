2eba6a21fa561e55b928dea56350832ce7a720b6
chewruixuan <ruixuanchew@gmail.com>
2024-10-13 00:21:54 +0800

    Fixed Nutrition

--- routes/routeNutrition.js
+++ routes/routeNutrition.js
@@ -5,13 +5,10 @@ const nutritionDBObject = new NutritionDB();
 
 function routeNutrition(app) {
     app.route('/nutritions')
-       .post(nutritionDBObject.addNutrition)
        .get(nutritionDBObject.getAllNutrition);
     
     app.route('/nutritions/:id')
       .get(nutritionDBObject.getNutritionById)
-       .put(nutritionDBObject.updateNutrition)
-       .delete(nutritionDBObject.deleteNutrition);
 }
 
 module.exports = { routeNutrition };
